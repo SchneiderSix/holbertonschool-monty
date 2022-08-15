203c86015a92b712cab1492ab52ec25a46857889
SchnedierSix <https://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@github.com>
2022-08-15 16:22:31 -0300

    token implementation

--- main.c
+++ main.c
@@ -2,5 +2,31 @@
 
 int main (int argc, char *argv[])
 {
-	
+	FILE *o;
+	char *buffer = NULL, *token;
+	size_t size 0;
+	stack_t node;
+	unsigned int line_number = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file");
+		exit(EXIT_FAILURE);
+	}
+	o = fopen(argv[1], "r");
+	if (!o)
+	{
+		fprintf(stderr, "Error: Can't open file %p", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&buffer, &size, stdin) != -1)
+	{
+		token = strtok(&buffer, "\n");
+		process(&buffer, node, line_number);
+		line_number++;
+	}
+	fclose(o);
+	free(buffer);
+	exit(99);
+
 }
