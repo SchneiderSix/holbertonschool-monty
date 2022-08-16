f75d8ccece1ee35f8874e0a8fea398d090ba934c
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-16 19:47:56 -0300

    Syntax fix morefunctions

--- morefunctions.c
+++ morefunctions.c
@@ -11,7 +11,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
-	temp = *stack
+	temp = *stack;
 	if (!stack)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
