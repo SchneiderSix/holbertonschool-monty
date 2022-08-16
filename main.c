2206a7877466f5ded3d7257abfa926d5d2367a41
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-16 15:04:08 -0300

    Getline attempt

--- main.c
+++ main.c
@@ -27,7 +27,8 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while ((rd = getline(&buffer, &size, o)) != -1)
+	rd = getline(&buffer, &size, o);
+	while (rd != -1)
 	{
 		line_number++;
 		opcode = strtok(buffer, " \t\n\r");
