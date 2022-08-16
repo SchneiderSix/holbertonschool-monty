2459f0480ee38db17a3742c90f299c4a2b3e40ba
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-15 22:45:11 -0300

    Betty fix

--- functions.c
+++ functions.c
@@ -9,35 +9,34 @@
  */
 void push(stack_t **node, char *numbers,  unsigned int line_number)
 {
-	stack_t *nn = NULL;
-	int i;
+        stack_t *nn = NULL;
+        int i;
 
-	if (!numbers)
-	{
-		printf("L%d: unknown instruction for push", line_number);
-		exit(EXIT_FAILURE);
-	}
-	for (i = 0; numbers[i] != '\0'; i++)
-	{
-		if (!isdigit(numbers[i]))
-		{
-			printf("L%d: unknown instruction for push", line_number);
-			exit(EXIT_FAILURE);
-		}
-	}
-	nn = malloc(sizeof(stack_t));
-	if (!nn)
-	{
-		printf("Error: malloc failed");
-		exit(EXIT_FAILURE);
-	}
-	nn->n = atoi(numbers);
-	nn->next = *node;
-	nn->prev = NULL;
-	if ((*node) != NULL)
-	{
-		(*node)->prev = nn;
-	}
-	(*node) = nn;
-
+        if (!numbers)
+        {
+                printf("L%d: unknown instruction for push", line_number);
+                exit(EXIT_FAILURE);
+        }
+        for (i = 0; numbers[i] != '\0'; i++)
+        {
+                if (!isdigit(numbers[i]))
+                {
+                        printf("L%d: unknown instruction for push", line_number);
+                        exit(EXIT_FAILURE);
+                }
+        }
+        nn = malloc(sizeof(stack_t));
+        if (!nn)
+        {
+                printf("Error: malloc failed");
+                exit(EXIT_FAILURE);
+        }
+        nn->n = atoi(numbers);
+        nn->next = *node;
+        nn->prev = NULL;
+        if ((*node) != NULL)
+        {
+                (*node)->prev = nn;
+        }
+        (*node) = nn;
 }
--- main.c
+++ main.c
@@ -9,43 +9,42 @@
  */
 int main(int argc, char *argv[])
 {
-	FILE *o;
-	char *buffer = NULL, *opcode, *numbers;
-	stack_t node = NULL;
-	unsigned int line_number = 0;
-	size_t size 0;
+        FILE *o;
+        char *buffer = NULL, *opcode, *numbers;
+        stack_t node = NULL;
+        unsigned int line_number = 0;
 
+        size_t size 0;
 
-	if (argc != 2)
-	{
-		printf("USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	o = fopen(argv[1], "r");
-	if (!o)
-	{
-		printf("Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (getline(&buffer, &size, stdin) != -1)
-	{
+        if (argc != 2)
+        {
+                printf("USAGE: monty file\n");
+                exit(EXIT_FAILURE);
+        }
+        o = fopen(argv[1], "r");
+        if (!o)
+        {
+                printf("Error: Can't open file %s\n", argv[1]);
+                exit(EXIT_FAILURE);
+        }
+        while (getline(&buffer, &size, stdin) != -1)
+        {
 
-		opcode = strtok(&buffer, " \t$");
-		if (strcmp(opcode, "push") == 0)
-		{
-			numbers = strtok(NULL, " \t$");
-			push(&node, numbers, line_number)
+                opcode = strtok(&buffer, " \t$");
+                if (strcmp(opcode, "push") == 0)
+                {
+                        numbers = strtok(NULL, " \t$");
+                        push(&node, numbers, line_number)
 
-		}
-		else
-		{
-			/*every other opcode less of course op push*/
-		}
-		line_number++;
+                }
+                else
+                {
+                        /*every other opcode less of course op push*/
+                }
+                line_number++;
 
-	}
-	fclose(o);
-	free(buffer);
-	exit(99);
-
+        }
+        fclose(o);
+        free(buffer);
+        exit(99);
 }
