ea06b66650e5e7641dd853eaa47e00751ebb8a2d
Rodrigo <5093@holbertonschool.com>
2022-08-16 15:23:57 -0700

    first draft pop function

---
+++ morefunctions.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+* _pop - removes the top element of the stack
+* @stack: stack head
+* @line_number: line num
+* Return: Void
+*/
+
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	temp = *stack
+	if (!stack)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+	exit(EXIT_FAILURE);
+	}
+	if (temp->next == NULL)
+		*stack = NULL;
+	else
+	{
+		*stack = temp->next;
+		temp->next->prev = NULL;
+	}
+	free(temp);
+}
