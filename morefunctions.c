c9f3a146e1e13b8c99044871e58e6ac01020ae82
Rodrigo <5093@holbertonschool.com>
2022-08-16 15:27:49 -0700

    functions mont

--- morefunctions.c
+++ morefunctions.c
@@ -15,7 +15,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 	if (!stack)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-	exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	if (temp->next == NULL)
 		*stack = NULL;
