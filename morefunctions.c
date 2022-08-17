32b7bec2d30c42c309f99438f00584eb728cc6a2
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-16 21:48:21 -0300

    Fix pop

--- morefunctions.c
+++ morefunctions.c
@@ -17,12 +17,7 @@ void _pop(stack_t ***stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	if (temp->next == NULL)
-	{
-		**stack = NULL;
-		return;
-	}
-	**stack = (**stack)->next;
-	(**stack)->prev = NULL;
-	free(temp);
+	temp = (**stack)->next;
+	free(**stack);
+	**stack = temp;
 }
